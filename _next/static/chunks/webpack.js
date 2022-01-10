24f3eaff2e8d15b948ff727effdcfab9795042af
Deven Parekh <deven.parekh@mail.mcgill.ca>
2022-01-10 10:21:43 -0500

    updated

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0057094907ead16ebd84";
+/******/ 	var hotCurrentHash = "e9a54ecc9d75c1777b7d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
